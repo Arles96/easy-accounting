da36acf482d7791647670ab2242b6abf102926aa
Arles96 <arles.cerrato@unitec.edu>
2020-11-04 02:44:03 -0600

    Agregando las funciones correspondientes de la base de datos de Exercises

---
+++ src/api/exercisesApi.js
@@ -0,0 +1,11 @@
+import Exercises from '../config/ExerciseDb';
+
+export const AddExercise = () => {};
+
+export const updateExercise = () => {};
+
+export const getExercises = () => {};
+
+export const getOneExercises = () => {};
+
+export const deleteExercises = () => {};
